ccbb410dbc2f36866962f6be613e2da162563bb6
lohitk1 <kamatham.lohit@gmail.com>
2025-02-06 06:44:07 -0500

    ADFASDF

--- requester/requester/wsgi.py
+++ requester/requester/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'requester.requester.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'requester.settings')
 
 application = get_wsgi_application()
